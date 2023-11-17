cc52289626f4dc12112b416d1f09a946ae31077f
Marcus <marcus.eshs@gmail.com>
2023-11-16 22:01:15 -0800

    add sprite to ui

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ data_dir = os.path.join(main_dir, "data")
 
 # Resource handlers
 
-
 def load_image(name, colorkey=None, scale=1):
     fullname = os.path.join(data_dir, name)
     image = pg.image.load(fullname)
@@ -43,31 +42,84 @@ def load_sound(name):
     return sound
 
 
+# classes for our game objects
+
 # class Player:
+class Sprite(pg.sprite.Sprite):
+    """moves a clenched fist on the screen, following the mouse"""
+
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)  # call Sprite initializer
+        self.image, self.rect = load_image("Sprite Template Front.png", -1)
+        self.fist_offset = (-235, -80)
+        self.punching = False
+
+    def update(self):
+        """move the fist based on the mouse position"""
+        pos = pg.mouse.get_pos()
+        self.rect.topleft = pos
+        self.rect.move_ip(self.fist_offset)
+        if self.punching:
+            self.rect.move_ip(15, 25)
+
+    def punch(self, target):
+        """returns true if the fist collides with the target"""
+        if not self.punching:
+            self.punching = True
+            hitbox = self.rect.inflate(-5, -5)
+            return hitbox.colliderect(target.rect)
+
+    def unpunch(self):
+        """called to pull the fist back"""
+        self.punching = False
+
 
 def main():
+
+    # initialize everything
     pg.init()
     screen = pg.display.set_mode((1280,600), pg.SCALED)
     pg.display.set_caption("Game Project 1")
     pg.mouse.set_visible(False)
+
+    # create the background
     background = pg.Surface(screen.get_size())
     background = background.convert()
     background.fill((170, 238, 187))
+
+    #put text on baclground
+    if pg.font:
+        font = pg.font.Font(None, 64)
+        text = font.render("Game", True, (10, 10, 10))
+        textpos = text.get_rect(centerx=background.get_width() / 2, y=10)
+        background.blit(text, textpos)
+
+    # display background
     screen.blit(background, (0, 0))
     pg.display.flip()
-    # Create variable containing sounds and sprites
+
+    # Prepare Game Objects
+    sprite = Sprite()
+    allsprites = pg.sprite.RenderPlain((sprite))
     clock = pg.time.Clock()
 
+    # Main Loop
     going = True
     while going:
         clock.tick(60)
 
+        # Handle Input Events
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 going = False
             elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                 going = False
+
+        allsprites.update()
+
+        # Draw Everything
         screen.blit(background, (0, 0))
+        allsprites.draw(screen)
         pg.display.flip()
 
     pg.quit()
