abb6ff0a208a33dc3308bbefc4fe9c66553c2fc5
Marcus <schumannmarcus15@gmail.com>
2024-02-17 16:23:31 -0800

    Revert "Added Camera class. Fixes needed."
    This reverts commit 7fea7c68c8ef4a221c1cbf3556b2fc204129c47d.

--- classes/player.py
+++ classes/player.py
@@ -1,132 +1,108 @@
 import pygame as pg
 from classes.resources import Resources
 
-
 # class Player:
 class Player(pg.sprite.Sprite):
-    """moves a clenched fist on the screen, following the mouse"""
-
-    def __init__(self, x, y):
-        super().__init__()
+   """moves a clenched fist on the screen, following the mouse"""
+   def __init__(self):
         res = Resources()
         pg.sprite.Sprite.__init__(self)  # call Sprite initializer
         self.image, self.rect = res.load_image("player", "SpriteTemplateFront.png", -1)
         self.speed = 5  # Speed of movement
-        self.rect.center = (x, y)
-        # self.rect.x = 960
-        # self.rect.y = 540
 
-    def input(self, counter):
-        keys2 = pg.KEYUP
-        keys = pg.key.get_pressed()
-        if keys[pg.K_a]:
-            self.rect.x -= self.speed
-            counter = self.leftWalk(self.rect.x, self.rect.y, counter)
-        elif keys[pg.K_d]:
-            self.rect.x += self.speed
-            counter = self.rightWalk(self.rect.x, self.rect.y, counter)
-        elif keys[pg.K_w]:
-            self.rect.y -= self.speed
-            counter = self.upWalk(self.rect.x, self.rect.y, counter)
-        elif keys[pg.K_s]:
-            self.rect.y += self.speed
-            counter = self.downWalk(self.rect.x, self.rect.y, counter)
-        elif keys2:
-            self.defaultPose(self.rect.x, self.rect.y)
+        self.rect.x = 960
+        self.rect.y = 540
 
-        return counter
+   def input(self, counter):
+      keys2 = pg.KEYUP
+      keys = pg.key.get_pressed()
+      if keys[pg.K_a]:
+         self.rect.x -= self.speed
+         counter = self.leftWalk(self.rect.x, self.rect.y,counter)
+      elif keys[pg.K_d]:
+         self.rect.x += self.speed
+         counter = self.rightWalk(self.rect.x, self.rect.y, counter)
+      elif keys[pg.K_w]:
+         self.rect.y -= self.speed
+         counter = self.upWalk(self.rect.x, self.rect.y, counter)
+      elif keys[pg.K_s]:
+         self.rect.y += self.speed
+         counter = self.downWalk(self.rect.x, self.rect.y, counter)
+      elif keys2:
+         self.defaultPose(self.rect.x, self.rect.y)
 
-    def leftWalk(self, x, y, counter):
-        res = Resources()
+      return counter
 
-        images = ['SpriteTemplateLeft.png', 'SpriteTemplateLeftWalk.png']
-        counter = (counter + 1) % len(images)
-        self.image, self.rect = res.load_image("player", images[counter], -1)
+   def leftWalk(self, x, y, counter):
+      res = Resources()
+      
+      images = ['SpriteTemplateLeft.png','SpriteTemplateLeftWalk.png']
+      counter = (counter + 1) % len(images)
+      self.image, self.rect = res.load_image("player", images[counter], -1)
 
-        self.rect.x = x
-        self.rect.y = y
+      self.rect.x = x 
+      self.rect.y = y
 
-        return counter
+      return counter
+   
+   def rightWalk(self, x, y, counter):
+      res = Resources()
 
-    def rightWalk(self, x, y, counter):
-        res = Resources()
+      images = ['SpriteTemplateRight.png','SpriteTemplateRightWalk.png']
+      counter = (counter + 1) % len(images)
+      self.image, self.rect = res.load_image("player", images[counter], -1)
 
-        images = ['SpriteTemplateRight.png', 'SpriteTemplateRightWalk.png']
-        counter = (counter + 1) % len(images)
-        self.image, self.rect = res.load_image("player", images[counter], -1)
+      self.rect.x = x
+      self.rect.y = y
 
-        self.rect.x = x
-        self.rect.y = y
+      return counter
+   
+   def upWalk(self, x, y, counter):
+      res = Resources()
 
-        return counter
+      images = ['SpriteTemplateBack.png','SpriteTemplateBackWalk1.png','SpriteTemplateBackWalk2.png']
+      counter = (counter + 1) % len(images)
+      self.image, self.rect = res.load_image("player", images[counter], -1)
 
-    def upWalk(self, x, y, counter):
-        res = Resources()
+      self.rect.x = x
+      self.rect.y = y
 
-        images = ['SpriteTemplateBack.png', 'SpriteTemplateBackWalk1.png', 'SpriteTemplateBackWalk2.png']
-        counter = (counter + 1) % len(images)
-        self.image, self.rect = res.load_image("player", images[counter], -1)
+      return counter
 
-        self.rect.x = x
-        self.rect.y = y
+   def downWalk(self, x, y, counter):
+      res = Resources()
+      
+      images = ['SpriteTemplateFront.png','SpriteTemplateWalk1.png','SpriteTemplateWalk2.png']
+      counter = (counter + 1) % len(images)
+      self.image, self.rect = res.load_image("player", images[counter], -1)
 
-        return counter
+      self.rect.x = x
+      self.rect.y = y
 
-    def downWalk(self, x, y, counter):
-        res = Resources()
+      return counter
 
-        images = ['SpriteTemplateFront.png', 'SpriteTemplateWalk1.png', 'SpriteTemplateWalk2.png']
-        counter = (counter + 1) % len(images)
-        self.image, self.rect = res.load_image("player", images[counter], -1)
+   def defaultPose(self, x, y):
+      res = Resources()
+      self.image, self.rect = res.load_image("player", "SpriteTemplateFront.png", -1)
 
-        self.rect.x = x
-        self.rect.y = y
+      self.rect.x = x
+      self.rect.y = y
 
-        return counter
-
-    def defaultPose(self, x, y):
-        res = Resources()
-        self.image, self.rect = res.load_image("player", "SpriteTemplateFront.png", -1)
-
-        self.rect.x = x
-        self.rect.y = y
-
-    # def update(self):
-    #     """move the fist based on the mouse position"""
-    #     pos = pg.mouse.get_pos()
-    #     self.rect.topleft = pos
-    #     self.rect.move_ip(self.fist_offset)
-    #     if self.punching:
-    #         self.rect.move_ip(15, 25)
+   # def update(self):
+   #     """move the fist based on the mouse position"""
+   #     pos = pg.mouse.get_pos()
+   #     self.rect.topleft = pos
+   #     self.rect.move_ip(self.fist_offset)
+   #     if self.punching:
+   #         self.rect.move_ip(15, 25)
 #
-# def punch(self, target):
-#     """returns true if the fist collides with the target"""
-#     if not self.punching:
-#         self.punching = True
-#         hitbox = self.rect.inflate(-5, -5)
-#         return hitbox.colliderect(target.rect)
+   # def punch(self, target):
+   #     """returns true if the fist collides with the target"""
+   #     if not self.punching:
+   #         self.punching = True
+   #         hitbox = self.rect.inflate(-5, -5)
+   #         return hitbox.colliderect(target.rect)
 #
-# def unpunch(self):
-#     """called to pull the fist back"""
-#     self.punching = False
-
-
-# class Player(pygame.sprite.Sprite):
-# def __init__(self, x, y):
-# super().__init__()
-# self.image = pygame.Surface((50, 50))
-# self.image.fill((255, 0, 0))
-# self.rect = self.image.get_rect()
-# self.rect.center = (x, y)
-# def update(self, target):
-# Adjust the camera to follow the target (player)
-# camera_x = -target.rect.x + self.width // 2
-# camera_y = -target.rect.y + self.height // 2
-
-# Clamp camera position to prevent it from going beyond the game boundaries
-# camera_x = min(0, camera_x)
-# camera_y = min(0, camera_y)
-
-# You can add additional logic here based on your game's requirements
-
-# self.camera = (camera_x, camera_y)
+   # def unpunch(self):
+   #     """called to pull the fist back"""
+   #     self.punching = False
--- main.py
+++ main.py
@@ -4,26 +4,20 @@ import pygame as pg
 # import classes
 from classes.player import Player
 from classes.resources import Resources
-from classes.camera import Camera
-
-WIDTH, HEIGHT = 800, 600
-GREEN = (170, 238, 187)
-FPS = 60
-
 
 def main():
     # initialize everything
     pg.init()
-    screen = pg.display.set_mode((1920, 1080), pg.SCALED)
+    screen = pg.display.set_mode((1920,1080), pg.SCALED)
     pg.display.set_caption("Game Project 1")
     pg.mouse.set_visible(False)
 
     # create the background
     background = pg.Surface(screen.get_size())
     background = background.convert()
-    background.fill(GREEN)
+    background.fill((170, 238, 187))
 
-    # put text on background
+    #put text on background
     if pg.font:
         font = pg.font.Font(None, 64)
         text = font.render("Game", True, (10, 10, 10))
@@ -38,51 +32,72 @@ def main():
     music = Resources()
     music.load_music('MyBrothersWife.mp3')
 
-    clock = pg.time.Clock()
-
-
     # Prepare Game Objects
-    sprite = Player(WIDTH // 2, HEIGHT // 2)
-    all_sprites = pg.sprite.Group(sprite)
-    #all_sprites.add(sprite)
-    camera = Camera(WIDTH, HEIGHT)
+    all_sprites = pg.sprite.Group()
+    sprite = Player()
+    all_sprites.add(sprite)
 
+    clock = pg.time.Clock()
 
     # Main Loop
     going = True
     counter = 0
-
+    
     while going:
-
+        clock.tick(60)
 
         # Handle Input Events
         for event in pg.event.get():
-            if event.type == pg.QUIT:
+           if event.type == pg.QUIT:
                 going = False
-            elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+           elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                 going = False
 
         counter = sprite.input(counter)
 
-        all_sprites.update()
-        camera.update(sprite)
-
         # Draw Everything
-        #screen.blit(background, (0, 0))
-        #all_sprites.draw(screen)
-
-        for sprite in all_sprites:
-            screen.blit(sprite.image, camera.apply(sprite))
-
+        screen.blit(background, (0, 0))
+        all_sprites.draw(screen)
         pg.display.flip()
-
-        clock.tick(FPS)
 
     pg.quit()
 
-
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Example file showing a circle moving on screen
 # import pygame
@@ -97,33 +112,33 @@ if __name__ == "__main__":
 # player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 # while running:
-# poll for events
-# pygame.QUIT event means the user clicked X to close your window
-# for event in pygame.event.get():
-# if event.type == pygame.QUIT:
-# running = False
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    # for event in pygame.event.get():
+        # if event.type == pygame.QUIT:
+            # running = False
 
-# fill the screen with a color to wipe away anything from last frame
-# screen.fill("black")
+    # fill the screen with a color to wipe away anything from last frame
+    # screen.fill("black")
 
-# pygame.draw.circle(screen, "red", player_pos, 40)
+    # pygame.draw.circle(screen, "red", player_pos, 40)
 
-# keys = pygame.key.get_pressed()
-# if keys[pygame.K_w]:
-# player_pos.y -= 300 * dt
-# if keys[pygame.K_s]:
-# player_pos.y += 300 * dt
-# if keys[pygame.K_a]:
-# player_pos.x -= 300 * dt
-# if keys[pygame.K_d]:
-# player_pos.x += 300 * dt
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_w]:
+        # player_pos.y -= 300 * dt
+    # if keys[pygame.K_s]:
+        # player_pos.y += 300 * dt
+    # if keys[pygame.K_a]:
+        # player_pos.x -= 300 * dt
+    # if keys[pygame.K_d]:
+        # player_pos.x += 300 * dt
 
-# flip() the display to put your work on screen
-# pygame.display.flip()
+    # flip() the display to put your work on screen
+    # pygame.display.flip()
 
-# limits FPS to 60
-# dt is delta time in seconds since last frame, used for framerate-
-# independent physics.
-# dt = clock.tick(60) / 1000
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    # dt = clock.tick(60) / 1000
 
 # pygame.quit()
